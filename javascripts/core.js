b16d16a02d7091afb577f4a5a92a57337a87f34b
wrapsjohn <johnachor@gmail.com>
2017-09-22 16:42:31 -0500

    changed console.log to console.table on the message object

--- javascripts/core.js
+++ javascripts/core.js
@@ -10,7 +10,7 @@ const ConvertMessage = require("./object-to-string");
 
 // CoreProcess :: Converts an object into an HTML string and prints it to the DOM
 const CoreProcess = (MessageObject) => {
-	console.log(MessageObject);
+	console.table(MessageObject);
 	let MessageBoard = document.getElementById("messageBoard");
 	let MessageString = ConvertMessage(MessageObject);
 	DomExporter(MessageString, MessageBoard);
