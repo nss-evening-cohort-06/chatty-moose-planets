f17f7de889a78b93dc1344f652e14b38530708ea
John Achor <johnachor@gmail.com>
2017-09-23 16:02:45 -0500

    fixed message deletion, disallowed entering empty messages

--- javascripts/backlog.js
+++ javascripts/backlog.js
@@ -8,7 +8,6 @@ const AddNewMessage = (MessageString) => {
   if (backlog.length === 21) {
     backlog.shift();
   }
-  console.log(backlog);
   return backlog;
 };
 
@@ -16,18 +15,17 @@ const AddNewMessage = (MessageString) => {
 // GetBacklog :: Returns the last X elements of the backlog, where X is the message limit.
 const GetBacklog = (MessageLimit) => {
   if (backlog.length < MessageLimit) {
-    console.log(backlog);
     return backlog;
   } else {
-    console.log(backlog);
     return backlog.slice(-MessageLimit);
   }  
 };
 
 // DeleteMessage :: Removes a string from the backlog array and returns the backlog.
-const DeleteMessage = (RemovedString) => {
-  backlog = backlog.filter(string => string.includes(RemovedString) === false);
+const DeleteMessage = (RemovedId) => {
   console.log(backlog);
+  console.log(typeof RemovedId, RemovedId);
+  backlog = backlog.filter(string => string.includes(RemovedId) === false);
   return backlog;
 };
 
